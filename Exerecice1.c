57956c9f5841b7e02b2d679ad74707a348fe4755
Abderrahmane Lahouasna <103077345+Mesteriwan82@users.noreply.github.com>
2023-08-03 18:06:17 +0100

    Add files via upload

---
+++ Exerecice1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+/* Exercice 1
+ Écrire  un programme en  C  qui permet  d’afficher le message Bonjour.
+ */
+int main()
+{
+    printf("Bonjour");
+    return 0;
+}
