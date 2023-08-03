6f2c877e00ed78817a43c05c8bcca1bb11ed8136
Abderrahmane Lahouasna <103077345+Mesteriwan82@users.noreply.github.com>
2023-08-03 18:07:23 +0100

    Add files via upload

---
+++ Exercice2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+/*Exercice 2
+Écrire  un programme en C permettant de saisir deux nombres et d'afficher leur produit.
+*/
+int main()
+{       int number1,number2,result;
+number1=29;
+number2=30;
+     result=number1*number2;
+    printf("result =%d\n",result);
+    return 0;
+}
